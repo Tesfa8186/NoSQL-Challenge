0c917c91d88e845169a040f8e4c11f38c8d1b409
Tesfa8186 <tesfa1978@gmail.com>
2023-03-29 20:51:36 +0800

    Update file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./user-controllers");
+const thoughtRoutes = require("./thought-controller");
 
 router.use("/users", userRoutes);
+router.use("/thoughts", thoughtRoutes);
 
 module.exports = router;
