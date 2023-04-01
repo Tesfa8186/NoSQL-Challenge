0d83fedb840cef2873350a1f3b39a91e6511ac54
Tesfa8186 <tesfa1978@gmail.com>
2023-04-01 08:48:35 +0800

    Updating files

---
+++ controllers/api/thought-controller.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+const Thought = require("../../models/Thought");
+
+// Get all thoughts
+router.get("/", (req, res) => {
+  Thought.find().then((result) => {
+    res.json(result);
+  });
+});
+
+// Get thought by ID
+router.get("/:id", (req, res) => {
+  Thought.findById(req.params.id).then((result) => {
+    res.json(result);
+  });
+});
+
+// Create thought
+router.post("/", (req, res) => {
+  console.log(req.body);
+  Thought.create(req.body).then((result) => {
+    res.json(result);
+  });
+});
+
+// Update thought by ID
+router.put("/:id", (req, res) => {
+  Thought.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  }).then((result) => {
+    res.json(result);
+  });
+});
+
+// Delete thought by ID
+router.delete("/:id", (req, res) => {
+  Thought.findByIdAndDelete(req.params.id).then((result) => {
+    res.json(result);
+  });
+});
+
+module.exports = router;
