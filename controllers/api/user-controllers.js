235ca8fca84ec10e683a4f84d46050048f5debe3
Tesfa8186 <tesfa1978@gmail.com>
2023-04-01 08:37:35 +0800

    Update files

--- controllers/api/user-controllers.js
+++ controllers/api/user-controllers.js
@@ -1,25 +1,28 @@
 const router = require("express").Router();
 const User = require("../../models/User");
 
+// Get all users
 router.get("/", (req, res) => {
   User.find().then((result) => {
     res.json(result);
   });
 });
 
+// Get a user by ID
 router.get("/:id", (req, res) => {
   User.findById(req.params.id).then((result) => {
     res.json(result);
   });
 });
 
+// Creat user
 router.post("/", (req, res) => {
   console.log(req.body);
   User.create(req.body).then((result) => {
     res.json(result);
   });
 });
-
+// Update user by ID
 router.put("/:id", (req, res) => {
   User.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -27,7 +30,7 @@ router.put("/:id", (req, res) => {
     res.json(result);
   });
 });
-
+// Delete user by ID
 router.delete("/:id", (req, res) => {
   User.findByIdAndDelete(req.params.id).then((result) => {
     res.json(result);
